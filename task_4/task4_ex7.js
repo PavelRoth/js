05f314b2c989b7633d57e17fc87f87b5f40d579d
PavelRoth <69809931+PavelRoth@users.noreply.github.com>
2021-12-17 03:13:53 +0200

    Delete task_4 directory

--- task_4/task4_ex7.js
+++
@@ -1,8 +0,0 @@
-let arr = [' hello darkness   my old   friend ', ' i\'ve come to   talk  with   you again    ']
-Array.from(arr)
-
-let trueArr = arr.map(function (x) {
-    return x.toString().replace(/ +/g, ' ').trim()
-})
-
-console.log(trueArr) 
