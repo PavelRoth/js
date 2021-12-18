40062ec64375c022a21683911a88c70be3f00ec6
PavelRoth <69809931+PavelRoth@users.noreply.github.com>
2021-12-19 00:14:56 +0200

    Update script.js

--- live weather/js/script.js
+++ live weather/js/script.js
@@ -6,8 +6,8 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Kramatorsk&appid=8b06514
         console.log(data);
         document.querySelector('.country').innerHTML = 'Country: ' + data.sys.country
         document.querySelector('.cityName').innerHTML = 'City name: ' + data.name
-        document.querySelector('.temperature').innerHTML = 'Temperature: ' + Math.floor((data.main.temp - 273))+ '&deg'
-        document.querySelector('.feelsLike').innerHTML = 'Feels like: ' + Math.floor((data.main.feels_like - 273))+ '&deg'
+        document.querySelector('.temperature').innerHTML = 'Temperature: ' + Math.round((data.main.temp - 273))+ '&deg'
+        document.querySelector('.feelsLike').innerHTML = 'Feels like: ' + Math.round((data.main.feels_like - 273))+ '&deg'
         document.querySelector('.wind').innerHTML = 'Wind: ' +   data.wind.speed + ' м/с'
     })
     .catch(function() {
